d628c1ac9a7269840d32eb2ac0f6873efc8d3ffa
Felipe Bagnato <felipebag@protonmail.com>
2020-10-11 20:12:19 -0300

    More bug fixes!
    The program does not skip when errors occurs anymore

--- backup.py
+++ backup.py
@@ -2,9 +2,9 @@ import os
 from shutil import rmtree
 
 dirHome = str(os.environ['HOME'])
-varError = 1
 
 def backupStart(pasta):
+   varError = 1
    os.chdir("{}/{}/".format(dirHome,pasta))
    os.mkdir("./Backup {}".format(pasta))
    print("Copiando os itens de {}".format(pasta))
@@ -16,14 +16,14 @@ def backupStart(pasta):
          print("\033[97mCopiando \033[32m{}".format(iten.replace(' ','\ ')))
    print('\033[97m')
    os.system('notify-send -i gtk-dialog-warning -u normal "Backup" "Digite a sua senha"')
-   global varError
+
    while(varError != 0):
        varError = os.system("7z a -p Backup\ {}.7z Backup\ {}".format(pasta,pasta))
        if(varError != 0):
            print("\033[0;31mA senha digitada esta incorreta!")
            print("Tente novamente\033[97m")
-   os.system("tar -cvf Backup\ {}.7z.tar Backup\ {}.7z".format(pasta,pasta))
 
+   os.system("tar -cvf Backup\ {}.7z.tar Backup\ {}.7z".format(pasta,pasta))
    rmtree("Backup {}".format(pasta))
    os.remove("Backup {}.7z".format(pasta))
 
