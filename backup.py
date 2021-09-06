7b925b159e31049f703193ef5de089ec318844e0
FeBagnato <felipebag@protonmail.com>
2021-09-06 19:20:39 -0300

    :sparkles: Otimizando o 7z e corrigindo a identação
    - usando a biblioteca py7zr ao inves de usar a ferramenta do sistema operacional
    - corrigindo a identação, usando um multiplo de quatro(E111)

--- backup.py
+++ backup.py
@@ -1,31 +1,35 @@
 import os
+import py7zr
 from shutil import rmtree
 
 dirHome = str(os.environ['HOME'])
 
 def backupStart(pasta):
-   varError = 1
-   os.chdir("{}/{}/".format(dirHome,pasta))
-   os.mkdir("./Backup {}".format(pasta))
-   print("Copiando os itens de {}".format(pasta))
-   for iten in os.listdir():
-      if iten.replace(' ',"\ ") == "Backup\ {}".format(pasta):
-         print('')
-      else:
-         os.system("cp -rf {} Backup\ {}".format(iten.replace(' ', "\ "),pasta))
-         print("\033[97mCopiando \033[32m{}".format(iten.replace(' ','\ ')))
-   print('\033[97m')
-   os.system('notify-send -i gtk-dialog-warning -u normal "Backup" "Digite a sua senha"')
+    passError = True
+    os.chdir("{}/{}/".format(dirHome, pasta))
+    os.mkdir("./Backup {}".format(pasta))
+    print("Copiando os itens de {}".format(pasta))
+    for iten in os.listdir():
+        if iten.replace(' ', "\ ") == "Backup\ {}".format(pasta):
+            print('')
+        else:
+            os.system("cp -rf {} Backup\ {}".format(iten.replace(' ', "\ "),pasta))
+            print("\033[97mCopiando \033[32m{}".format(iten.replace(' ', '\ ')))
+    print('\033[97m')
+    os.system('notify-send -i gtk-dialog-warning -u normal "Backup" "Digite a sua senha"')
 
-   while(varError != 0):
-       varError = os.system("7z a -p Backup\ {}.7z Backup\ {}".format(pasta,pasta))
-       if(varError != 0):
-           print("\033[0;31mA senha digitada esta incorreta!")
-           print("Tente novamente\033[97m")
+    while(passError):
+        senha = str(input("Digite a senha: "))
 
-   os.system("tar -cvf Backup\ {}.7z.tar Backup\ {}.7z".format(pasta,pasta))
-   rmtree("Backup {}".format(pasta))
-   os.remove("Backup {}.7z".format(pasta))
+        if(senha == str(input("Digite a senha novamente: "))):
+            with py7zr.SevenZipFile("Backup {}.7z".format(pasta), 'w', password=senha) as backup:
+                backup.writeall("Backup {}/".format(pasta))
+            passError = False
+        else:
+            print("\033[0;31mA senha esta incorreta!")
+            print("Tente novamente\n\033[97m")
+
+    rmtree("Backup {}".format(pasta))
 
 
 backupStart("Documentos")
