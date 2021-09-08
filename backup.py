ea8e20e482b0a9a266ef406218d48fb1e5e90f18
FeBagnato <felipebag@protonmail.com>
2021-09-08 12:40:01 -0300

    :sparkles: Pergunta pela senha só uma vez
    Nao pergunta mais pela senha a cada diretório comprimido

--- backup.py
+++ backup.py
@@ -5,8 +5,8 @@ from shutil import rmtree
 
 dirHome = str(os.environ['HOME'])
 
+
 def backupStart(pasta):
-    passError = True
     os.chdir("{}/{}/".format(dirHome, pasta))
     os.mkdir("./Backup {}".format(pasta))
     print("Copiando os itens de {}".format(pasta))
@@ -17,30 +17,25 @@ def backupStart(pasta):
             os.system("cp -rf {} Backup\ {}".format(iten.replace(' ', "\ "),pasta))
             print("\033[97mCopiando \033[32m{}".format(iten.replace(' ', '\ ')))
     print('\033[97m')
-    os.system('notify-send -i gtk-dialog-warning -u normal "Backup" "Digite a sua senha"')
 
-    while(passError):
-        senha = getpass("Digite a senha: ")
-
-        if(senha == getpass("Digite a senha novamente: ")):
-            with py7zr.SevenZipFile("Backup {}.7z".format(pasta), 'w', password=senha) as backup:
-                backup.writeall("Backup {}/".format(pasta))
-            passError = False
-        else:
-            print("\033[0;31mA senha esta incorreta!")
-            print("Tente novamente\n\033[97m")
-
+    with py7zr.SevenZipFile("Backup {}.7z".format(pasta), 'w', password=senha) as backup:
+        backup.writeall("Backup {}/".format(pasta))
     rmtree("Backup {}".format(pasta))
 
 
-backupStart("Documentos")
+passError = True
+while(passError):
+    senha = getpass("Digite a senha: ")
 
-backupStart("Downloads")
+    if(senha == getpass("Digite a senha novamente: ")):
+        backupStart("Documentos")
+        backupStart("Downloads")
+        backupStart("Desktop")
+        backupStart("Imagens")
+        backupStart("Música")
+        backupStart("Vídeos")
 
-backupStart("Desktop")
-
-backupStart("Imagens")
-
-backupStart("Música")
-
-backupStart("Vídeos")
+        passError = False
+    else:
+        print("\033[0;31mA senha esta incorreta!")
+        print("Tente novamente\n\033[97m")
