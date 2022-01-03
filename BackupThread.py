40756a80e0842a82c55497a2d6967564d8a08091
FeBagnato <felipebag@protonmail.com>
2022-01-03 03:28:09 -0300

    Criando a classe BackupThread
    Otimizando o script com o uso de Threads, para assim, diminuir o tempo que leva para terminar os backups

---
+++ BackupThread.py
@@ -0,0 +1,26 @@
+import threading
+import os
+import py7zr
+from shutil import rmtree
+
+class BackupThread(threading.Thread):
+    def __init__(self, pasta, senha):
+        self.pasta = pasta
+        self.senha = senha
+        self.dirHome = str(os.environ['HOME'])
+
+    def run(self):
+        os.chdir(f"{self.dirHome}/{self.pasta}/")
+        os.mkdir(f"./Backup {self.pasta}")
+        print(f"Copiando os itens de {self.self.pasta}")
+        for iten in os.listdir():
+            if iten.replace(' ', "\ ") == f"Backup\ {self.pasta}":
+                print('')
+            else:
+                os.system(f"cp -rf '{iten}' Backup\ {self.pasta}")
+                print(f"\033[97mCopiando \033[32m{iten}")
+        print('\033[97m')
+
+        with py7zr.SevenZipFile(f"Backup {self.pasta}.7z", 'w', password=self.senha) as backup:
+            backup.writeall(f"Backup {self.pasta}/")
+        rmtree(f"Backup {self.pasta}")
